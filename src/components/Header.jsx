fe4298ce7b03b9a0ec45e28351bf3ba97e1bba52
Liliana Del Carmen Chora Monar <116900601+lilichora@users.noreply.github.com>
2024-02-27 23:33:11 -0500

    aporte Liliana Chora

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { AppBar, Toolbar, Typography } from "@mui/material";
 
 const Header = () => {
@@ -7,6 +8,125 @@ const Header = () => {
         <Typography variant="h6">Online Learning Platform</Typography>
       </Toolbar>
     </AppBar>
+=======
+import React, { useState, useEffect } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  MenuItem,
+} from "@mui/material";
+import Courses from "../pages/Courses"; // Ajusta la ruta según la ubicación real del archivo Courses.jsx
+import { getCoursesFromFirestore } from "../api/coursesApi";
+
+const Header = () => {
+  const [openDialog, setOpenDialog] = useState(false);
+  const [formData, setFormData] = useState({
+    nombre: "",
+    correo: "",
+    curso: "",
+  });
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const coursesData = await getCoursesFromFirestore();
+      const formattedCourses = coursesData.map((course) => ({
+        ...course,
+        create: course.create.toDate(),
+      }));
+      setCourses(formattedCourses);
+    };
+
+    fetchCourses();
+  }, []);
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log("Datos enviados:", formData);
+    handleCloseDialog();
+  };
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Online Learning Platform</Typography>
+          <div style={{ marginLeft: "auto" }}>
+            <Button color="inherit" onClick={handleOpenDialog}>
+              Inscribirse
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle>Inscripción a nuevo curso</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Nombre"
+            fullWidth
+            margin="dense"
+            name="nombre"
+            value={formData.nombre}
+            onChange={handleInputChange}
+          />
+          <TextField
+            label="Correo electrónico"
+            fullWidth
+            margin="dense"
+            name="correo"
+            value={formData.correo}
+            onChange={handleInputChange}
+          />
+          <TextField
+            select
+            label="Curso"
+            fullWidth
+            margin="dense"
+            name="curso"
+            value={formData.curso}
+            onChange={handleInputChange}
+          >
+            {courses.map((course) => (
+              <MenuItem key={course.id} value={course.title}>
+                {course.title}
+              </MenuItem>
+            ))}
+          </TextField>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleSubmit} color="primary">
+            Enviar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+>>>>>>> Stashed changes
   );
 };
 
